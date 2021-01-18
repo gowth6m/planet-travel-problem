272f5581d331d6bd01db7b0fd063fb169e093e81
Gowtham <gowtham4n@gmail.com>
2021-01-18 23:16:33 +0000

    added path finding algo

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 # Question 1 - Planet Problem
 
-# Coordinate of Zearth
-import math
-import fileinput
-
 EARTH_COORDINATE = [0.0, 0.0, 0.0]
 ZEARTH_COORDINATE = []
 STATION_COORDINATES = []
-ASSIGNED_COORDINATES = {0: EARTH_COORDINATE}
+ASSIGNED_COORDINATES = {}
+
+
+def input_from_file(file):
+    """
+    Gets data from text file
+    :param file: name of the text file
+    :return: none
+    """
+    lines = open(file).read().split('\n')
+    global ZEARTH_COORDINATE
+    ZEARTH_COORDINATE = line_to_coord(lines[0])
+    if not (1 <= int(lines[1]) <= 2000):
+        raise Exception('Invalid number of stations')
+    # Get coordinates of stations
+    for i in range(len(lines)):
+        if (i == 0) or (i == 1):
+            pass
+        else:
+            STATION_COORDINATES.append(line_to_coord(lines[i]))
+
+
+def line_to_coord(line):
+    """
+    Takes a string and turns it into a list that represents coordinates.
+    :param line: string of coordinate
+    :return: a list of floats that represents a coordinate
+    """
+    coords = []
+    for i in line.split():
+        i = float(i)
+        if (i < -10000.0) or (i > 10000.0):  # Validates coordinate values
+            raise Exception('Invalid coordinate value')
+        coords.append(i)
+    if len(coords) > 3:  # Checks if there are only 3 numbers in a coordinate
+        raise Exception('Invalid coordinate value')
+    return coords
 
 
 def ask_for_input():
@@ -55,6 +87,7 @@ def assign_nodes_to_coordinates():
     :return: null
     """
     counter = 1
+    ASSIGNED_COORDINATES[0] = EARTH_COORDINATE
     for i in STATION_COORDINATES:
         ASSIGNED_COORDINATES[counter] = i
         counter += 1
@@ -63,22 +96,15 @@ def assign_nodes_to_coordinates():
 
 def get_weighted_graph():
     """
-    Format of the graph dictionary = { node_1 : [[node_2, weight_1&2],[node_3, weight_1&3]] }
+    Format of the graph dictionary = {node_1 : {node_2: weight_1&2, node_3: weight_1&3}}
     :return: a weighted graph of stations including Earth and Zearth.
     """
     graph = {}
-    final_node = len(ASSIGNED_COORDINATES)  # Zearth's node
     for i in ASSIGNED_COORDINATES.keys():
-        if i == 0:
-            graph[0] = [0, ASSIGNED_COORDINATES[0]]  # setting 0th node as earth's coordinate
-        if i == final_node:
-            graph[final_node] = [final_node, ASSIGNED_COORDINATES[final_node]]
-
-        graph[i] = []
+        graph[i] = {}
         for j in ASSIGNED_COORDINATES.keys():
             if j != i:
-                graph[i].append([j, get_weight(ASSIGNED_COORDINATES[i], ASSIGNED_COORDINATES[j])])
-    # print(graph)
+                graph[i].update({j: get_weight(ASSIGNED_COORDINATES[i], ASSIGNED_COORDINATES[j])})
     return graph
 
 
@@ -92,33 +118,46 @@ def get_weight(node1, node2):
     x_value = (node1[0] - node2[0]) ** 2
     y_value = (node1[1] - node2[1]) ** 2
     z_value = (node1[2] - node2[2]) ** 2
-    weight = math.sqrt(x_value + y_value + z_value)
+    weight = (x_value + y_value + z_value) ** (1/2)
     return weight
 
 
-def find_safest_path_distance():
-    pathDistance = 0
-    return pathDistance
+def find_safest_path_distance(graph, start, finish):
+    max_distance = {}
+    unseenNodes = graph
+    infinity = float("inf")
+    for node in unseenNodes:  # Initiates the minimum maximum distance for each node to be infinity
+        max_distance[node] = infinity
+    max_distance[start] = 0
 
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:  # Finding the minimum weighted node so far
+            if minNode is None:  # Base case
+                minNode = node
+            elif max_distance[node] < max_distance[minNode]:
+                minNode = node
 
-# def checkForInputFile():
-#     # print command line arguments
-#     args = []
-#     for arg in sys.argv[1:]:
-#         args.append(arg)
-#         print(args)
-#     if args[0] == '<':
-#         print("USING INPUT FILE" + args[1])
+        for childNode, weight in graph[minNode].items():
+            if max(weight, max_distance[minNode]) < max_distance[childNode]:  # Takes maximum of weights along path
+                max_distance[childNode] = max(weight, max_distance[minNode])
+        unseenNodes.pop(minNode)  # Breaks loop when finished
+
+    return max_distance[finish]
 
 
 if __name__ == '__main__':
-    try:
-        ask_for_input()
-        assign_nodes_to_coordinates()
-    except KeyboardInterrupt:
-        pass
-    print(get_weighted_graph())
-    # print(STATION_COORDINATES)
-    # print(getWeight([2, 3.5, 2], [2, 12, 213]))
-    print("Dictionary of nodes: ", ASSIGNED_COORDINATES)
-    # print("Zearth Coordinate: ",ZEARTH_COORDINATE)
+    # try:
+    #     ask_for_input()
+    #     assign_nodes_to_coordinates()
+    # except KeyboardInterrupt:
+    #     pass
+    filename = 'input.txt'  # Load input file
+    input_from_file(filename)
+    assign_nodes_to_coordinates()
+    world = get_weighted_graph()
+    print(world)
+    print("====TEST====")
+    # test_find_path(world, 0, len(world) - 1)
+    answer = find_safest_path_distance(world, 0, len(world) - 1)
+    print(answer)
